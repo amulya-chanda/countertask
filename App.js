2d41f89299fd6b06a5201d14ff765eb8d26d539f
amulya <amulya@divami.com>
2023-02-22 16:38:52 +0530

    updated

---
+++ App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import React, { useState } from "react";
+
+function App() {
+  let [count,setcount]=useState(0)
+  return(
+    <div className='counter'>
+      <h1>COUNTER</h1>
+      {count}
+      <div className='onclick_event'>
+        <button onClick={()=>setcount(count+=1)}>increment</button>
+        <button onClick={()=>setcount(count=0)}>Reset</button>
+        <button onClick={()=>setcount(count-=1)}>decrement</button>
+      </div>
+    </div>
+
+  )
+    
+  
+}
+export default App;
